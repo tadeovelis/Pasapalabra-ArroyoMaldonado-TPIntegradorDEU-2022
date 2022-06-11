291e5ee4c57fa1111f882be629b7d42a75962bf7
tadeo <tadeo@pop-os.localdomain>
2022-06-10 21:08:21 -0300

    Mejorar el estilo del header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,61 @@ import { Grid } from "@mui/material"
 import { Button } from "@mui/material"
 import { Link } from "react-router-dom"
 
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+
+import '../App.css'
+import styled from "@emotion/styled";
+
+const HeaderButton = styled(Button)({
+    fontSize: 20,
+    padding: '15px 35px',
+    borderRadius: '0 0 10px 10px'
+});
+
+const espacioIconos = 1;
+
 export default function Header() {
     return (
         <div>
-            <Grid container spacing={5} justifyContent="center">
-                <Grid
-                    item
-                >
-                    <Button
+            <Grid
+                container
+                spacing={5}
+                justifyContent="center"
+            >
+                <Grid item>
+                    <HeaderButton
                         component={Link}
                         variant="contained"
                         to="/como-jugar"
+                        size="large"
+                        startIcon={<HelpOutlineIcon sx={{ mr: espacioIconos }}/>}
                     >
                         ¿Cómo jugar?
-                    </Button>
+                    </HeaderButton>
                 </Grid>
                 <Grid item>
-                    <Button
+                    <HeaderButton
                         component={Link}
+                        size="large"
                         variant="contained"
                         to="/glosario"
+                        startIcon={<ImportContactsIcon sx={{ mr: espacioIconos }}/>}
                     >
                         Glosario
-                    </Button>
+                    </HeaderButton>
                 </Grid>
                 <Grid item>
-                    <Button
+                    <HeaderButton
                         component={Link}
                         variant="contained"
+                        size="large"
                         to="/acerca-de-la-app"
+                        startIcon={<PeopleOutlineIcon sx={{ mr: espacioIconos }}/>}
                     >
                         Acerca de la app
-                    </Button>
+                    </HeaderButton>
                 </Grid>
             </Grid>
         </div>
