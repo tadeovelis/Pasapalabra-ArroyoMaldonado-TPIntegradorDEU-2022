b1275033a314e9c83bdaba9490d54e6b7acb4429
tadeo <tadeovelis@gmail.com>
2022-06-14 12:21:18 -0300

    Hacer que el glosario lea las palabras del json

--- src/components/SeccionesComplementarias/Glosario.js
+++ src/components/SeccionesComplementarias/Glosario.js
@@ -1,20 +1,12 @@
-import { Box } from "@mui/system"
-import { Typography } from "@mui/material"
 import SeccionComplementaria from "./SeccionComplementaria"
 
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
 import BoxItemSeccionComplementaria from "./BoxItemSeccionComplementaria"
 
-const espacioIconos = 2;
+import palabras from '../../data/palabras.json'
 
-const palabras = [
-    { 'titulo': 'Inundación', 'definicion': 'Las inundaciones son desbordamientos de agua temporales hacia terrenos que normalmente están secos. Las inundaciones son el tipo de desastre natural más común en los Estados Unidos. El no desalojar las zonas inundadas o entrar en las aguas de inundación pueden causar lesiones o muerte.' },
-    { 'titulo': 'Inundación', 'definicion': 'Las inundaciones son desbordamientos de agua temporales hacia terrenos que normalmente están secos. Las inundaciones son el tipo de desastre natural más común en los Estados Unidos. El no desalojar las zonas inundadas o entrar en las aguas de inundación pueden causar lesiones o muerte.' },
-    { 'titulo': 'Inundación', 'definicion': 'Las inundaciones son desbordamientos de agua temporales hacia terrenos que normalmente están secos. Las inundaciones son el tipo de desastre natural más común en los Estados Unidos. El no desalojar las zonas inundadas o entrar en las aguas de inundación pueden causar lesiones o muerte.' },
-    { 'titulo': 'Inundación', 'definicion': 'Las inundaciones son desbordamientos de agua temporales hacia terrenos que normalmente están secos. Las inundaciones son el tipo de desastre natural más común en los Estados Unidos. El no desalojar las zonas inundadas o entrar en las aguas de inundación pueden causar lesiones o muerte.' },
-    { 'titulo': 'Inundación', 'definicion': 'Las inundaciones son desbordamientos de agua temporales hacia terrenos que normalmente están secos. Las inundaciones son el tipo de desastre natural más común en los Estados Unidos. El no desalojar las zonas inundadas o entrar en las aguas de inundación pueden causar lesiones o muerte.' },
-    { 'titulo': 'Inundación', 'definicion': 'Las inundaciones son desbordamientos de agua temporales hacia terrenos que normalmente están secos. Las inundaciones son el tipo de desastre natural más común en los Estados Unidos. El no desalojar las zonas inundadas o entrar en las aguas de inundación pueden causar lesiones o muerte.' },
-]
+
+const espacioIconos = 2;
 
 export default function Glosario() {
     return (
@@ -25,7 +17,7 @@ export default function Glosario() {
                 /* Algunas palabras hardcodeadas a modo de ejemplo */
                 palabras.map(p =>
                     <BoxItemSeccionComplementaria
-                        titulo={p.titulo}
+                        titulo={p.palabra}
                         contenido={p.definicion}
                     />
                 )
