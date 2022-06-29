520a0af678be641e85e37ef3f307fae49fc8e782
tadeo <tadeovelis@gmail.com>
2022-06-29 13:51:27 -0300

    Terminar lo básico de la sección Cómo jugar

--- src/components/SeccionesComplementarias/AcercaDeLaApp.js
+++ src/components/SeccionesComplementarias/AcercaDeLaApp.js
@@ -14,11 +14,24 @@ export default function AcercaDeLaApp() {
             contenido={
                 <>
                     <BoxItemSeccionComplementaria
+                        titulo="¿De qué trata?"
+                        contenido={
+                            <p>
+                                Este juego está basado en el formato del programa de televisión
+                                <i> "Pasapalabras"</i>, el cual, a su vez, es una versión adaptada del original británico
+                                <i> "The Alphabet Game"</i>. Este juego pone a prueba tus conocimientos sobre la lengua utilizada y sobre
+                                conceptos de cualquier índole, de una forma dinámica y muy entretenida.
+                            </p>
+                        }
+                    />
+                    <BoxItemSeccionComplementaria
                         titulo="¿Quiénes somos?"
                         contenido={
                             <>
-                                "Somos..."<br />
-                                <b>Tadeo Velis, Sebastián Perri y Mariano Gandin</b>.
+                                <p>
+                                    "Somos..."<br />
+                                    <b>Tadeo Velis, Sebastián Perri y Mariano Gandin</b>.
+                                </p>
                             </>
                         }
                     />
@@ -27,11 +40,15 @@ export default function AcercaDeLaApp() {
                         titulo="¿Cómo surgió?"
                         contenido={
                             <>
-                                Esta aplicación la diseñamos y desarrollamos para la materia Diseño de Experiencia de Usuario de la Facultad de Informática, UNLP, en 2022. En esta materia se prioriza el desarrollo de aplicaciones pensando en el usuario para lograr la mejor experiencia posible. Esto no solo incluye interfaces más simples y fáciles de entender, sino también interacciones más intuitivas y accesibilidad para todas las personas, de todas las edades y de todas las condiciones.
-                                <br /><br />
-                                La aplicación se diseñó en el marco del proyecto de difusión y educación promovido por distintas facultades de la UNLP, sobre las inundaciones de la región de La Plata. Específicamente, nos centramos en la de 2013 y en la región del Arroyo Maldonado.
-                                <br /><br />
-                                Se ha realizado un extenso y muy valioso trabajo de investigación. Si estás interesado y querés saber más, hacé click en el botón.
+                                <p>
+                                    Esta aplicación fue diseñada y desarrollada para la materia Diseño de Experiencia de Usuario de la Facultad de Informática, UNLP, en 2022. En esta materia se prioriza el desarrollo de aplicaciones pensando en el usuario para lograr la mejor experiencia posible. Esto no solo incluye interfaces más simples y fáciles de entender, sino también interacciones más intuitivas y accesibilidad para todas las personas, de todas las edades y de todas las condiciones.
+                                </p>
+                                <p>
+                                    La aplicación se diseñó en el marco del proyecto de difusión y educación promovido por distintas facultades de la UNLP, sobre las inundaciones de la región de La Plata. Específicamente, nos centramos en la de 2013 y en la región del Arroyo Maldonado.
+                                </p>
+                                <p>
+                                    Se ha realizado un extenso y muy valioso trabajo de investigación. Si estás interesado y querés saber más, hacé click en el botón.
+                                </p>
                             </>
                         }
                         extra={
@@ -53,8 +70,7 @@ export default function AcercaDeLaApp() {
                         titulo="Información de contacto"
                         contenido={
                             <>
-                                Si encontraste algún error, querés sugerir alguna palabra, alguna corrección o por cualquier otro motivo que te quieras contactar con nosotros, no dudes en escribirnos un mail. Te dejamos nuestras direcciones de correo electrónico, nos podés enviar un mail a los tres:
-                                <br /><br />
+                                <p>Si encontraste algún error, querés sugerir alguna palabra, alguna corrección o por cualquier otro motivo que te quieras contactar con nosotros, no dudes en escribirnos un mail. Te dejamos nuestras direcciones de correo electrónico, nos podés enviar un mail a los tres:</p>
                                 Tadeo Velis: <b>tadeo.velis@gmail.com</b><br />
                                 Sebastián Perri: <b>srperri@gmail.com</b><br />
                                 Mariano Gandin: <b>mariano.rng@hotmail.com</b>
--- src/components/SeccionesComplementarias/ComoJugar.js
+++ src/components/SeccionesComplementarias/ComoJugar.js
@@ -2,6 +2,12 @@ import SeccionComplementaria from "./SeccionComplementaria"
 
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import BoxItemSeccionComplementaria from "./BoxItemSeccionComplementaria"
+import { Grid, Typography } from "@mui/material";
+
+import pasapalabraImg from "../../img/PasapalabraButton.png";
+import confirmarImg from "../../img/ConfirmarButton.png";
+import inputImg from "../../img/Input.png";
+import { Box } from "@mui/system";
 
 const espacioIconos = 2;
 
@@ -10,13 +16,85 @@ export default function ComoJugar() {
     return (
         <SeccionComplementaria
             titulo="Cómo jugar"
-            icono={<HelpOutlineIcon sx={{ mr: espacioIconos, fontSize: 30 }}/>}
-            contenido={
+            icono={<HelpOutlineIcon sx={{ mr: espacioIconos, fontSize: 30 }} />}
+            contenido={<>
                 <BoxItemSeccionComplementaria
-                    titulo="¿Cuáles son las reglas?"
-                    contenido="Para jugar hay que..."
+                    titulo="¿Cuál es la mecánica del juego?"
+                    contenido={
+                        <>
+                            <p>
+                                Se presentarán las letras del abecedario, en orden y dispuestas en forma de círculo. Cada letra tendrá asignada una palabra.
+                                Tu tarea es acertar las palabras leyendo o escuchando sus definiciones. ¡Tendrás [X] segundos!
+                            </p>
+                            <p>
+                                En cada letra, si tenés una palabra en mente, la vas a ingresar en el campo de texto:
+                                <br></br>
+                                <Box sx={{ mt: 2, mb: 4 }}>
+                                    <img
+                                        src={inputImg}
+                                        width="250px"
+                                    />
+                                </Box>
+                                Pero podés tomar alguna de las dos siguientes decisiones:
+                                <Grid container sx={{ my: 3 }} >
+                                    <Grid item container spacing={3} sx={{ mb: 1 }} alignItems="center">
+                                        <Grid item>
+                                            <img
+                                                src={pasapalabraImg}
+                                                width="70px"
+                                            />
+                                        </Grid>
+                                        <Grid item>
+                                            <b>¡Pasapalabra!</b>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item>
+                                        <span>No necesitás responder, pasás directamente a la siguiente. De todas formas, cuando el rosco dé toda la vuelta, te va a tocar de nuevo, así que ¡recordá la definición!</span>
+                                    </Grid>
+                                </Grid>
+                                <Grid container sx={{ my: 3 }} >
+                                    <Grid item container spacing={3} sx={{ mb: 1 }} alignItems="center">
+                                        <Grid item>
+                                            <img
+                                                src={confirmarImg}
+                                                width="70px"
+                                            />
+                                        </Grid>
+                                        <Grid item>
+                                            <b>Confirmar</b>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item>
+                                        <span>Confirmás lo que ingresaste en el campo de texto. ¡Si es correcta acertás! Si no, es un error.</span>
+                                    </Grid>
+                                </Grid>
+                            </p>
+                            <p>
+                                Si llegás a la Z y todavía tenés alguna letra por responder, el rosco vuelve a comenzar.
+                            </p>
+                        </>
+                    }
                 />
-            }
+                <BoxItemSeccionComplementaria
+                    titulo="¿Cuándo termina? ¿Cómo se gana?"
+                    contenido={<>
+                        <p>
+                            El juego puede terminar por alguna de las siguientes dos razones:
+                        </p>
+                        <ul>
+                            <li>
+                                Contestás bien todas las palabras
+                            </li>
+                            <li>
+                                Se termina el tiempo
+                            </li>
+                        </ul>
+                        <p>
+                            Para ganar deberías responder bien todas las preguntas, pero si no respondiste todas, ¡no pasa nada! Vas a poder ver cuántas respondiste bien, lo importante es aprender y divertirse jugando.
+                        </p>
+                    </>}
+                />
+            </>}
         />
     )
 }
